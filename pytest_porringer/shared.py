36ba408cb10c2e62ba488c3abd957a94f01d16ff
Asher Norland <asher.norland@gmail.com>
2024-01-11 19:21:41 -0500

    Import `pytest_synodic`

--- pytest_porringer/shared.py
+++ pytest_porringer/shared.py
@@ -1,16 +1,19 @@
 """Shared data between the exposed fixtures"""
 
 from abc import ABCMeta
-from importlib.metadata import entry_points
-from typing import Generic
+from typing import LiteralString, cast
 
 import pytest
-from porringer_core.plugin_schema.environment import EnvironmentT
+from porringer_core.plugin_schema.environment import Environment, EnvironmentT
 from porringer_core.schema import PluginT
-from synodic_utilities.utility import canonicalize_type
+from pytest_synodic.plugin import BaseTests as SynodicBaseTests
+from pytest_synodic.plugin import IntegrationTests as SynodicBaseIntegrationTests
+from pytest_synodic.plugin import UnitTests as SynodicBaseUnitTests
+
+from pytest_porringer.variants import environment_variants
 
 
-class BaseTests(Generic[PluginT], metaclass=ABCMeta):
+class BaseTests[PluginT](SynodicBaseTests, metaclass=ABCMeta):
     """Shared testing information for all plugin test classes."""
 
     @pytest.fixture(name="plugin_type", scope="session")
@@ -19,62 +22,26 @@ class BaseTests(Generic[PluginT], metaclass=ABCMeta):
 
         raise NotImplementedError("Override this fixture")
 
-    @pytest.fixture(name="todo", scope="session")
-    def fixture_todo(self) -> None:
-        """A required testing hook that allows type generation"""
-
-
-class BaseIntegrationTests(Generic[PluginT], metaclass=ABCMeta):
-    """Integration testing information for all plugin test classes"""
-
-    def test_entry_point(self, plugin_type: type[PluginT]) -> None:
-        """Verify that the plugin was registered
-
-        Args:
-            plugin_type: The type to register
-        """
-        group = canonicalize_type(plugin_type).group
-
-        types = []
-        for entry in list(entry_points(group=f"porringer.{group}")):
-            types.append(entry.load())
-
-        assert plugin_type in types
-
-    def test_name(self, plugin_type: type[PluginT]) -> None:
-        """Verifies the the class name allows name extraction
-
-        Args:
-            plugin_type: The type to register
-        """
-        normalized = canonicalize_type(plugin_type)
-
-        assert normalized.group != ""
-        assert normalized.name != ""
-
-
-class BaseUnitTests(Generic[PluginT], metaclass=ABCMeta):
-    """Unit testing information for all plugin test classes"""
-
-    def test_information(self, plugin_type: type[PluginT]) -> None:
+    @pytest.fixture(name="plugin_group_name", scope="session")
+    def fixture_plugin_group_name(self) -> LiteralString:
         """_summary_
 
-        Args:
-            plugin_type: _description_
+        Returns:
+            _description_
         """
 
-        assert plugin_type.information()
-
-    def test_plugin_construction(self, plugin: PluginT) -> None:
-        """Verifies that the plugin being tested can be constructed
-
-        Args:
-            plugin: The data plugin fixture
-        """
-        assert plugin
+        return "porringer"
 
 
-class PluginTests(BaseTests[PluginT], Generic[PluginT], metaclass=ABCMeta):
+class BaseIntegrationTests[PluginT](SynodicBaseIntegrationTests, metaclass=ABCMeta):
+    """Integration testing information for all plugin test classes"""
+
+
+class BaseUnitTests[PluginT](SynodicBaseUnitTests, metaclass=ABCMeta):
+    """Unit testing information for all plugin test classes"""
+
+
+class PluginTests[PluginT](BaseTests[PluginT], metaclass=ABCMeta):
     """Testing information for basic plugin test classes."""
 
     @staticmethod
@@ -98,13 +65,31 @@ class PluginTests(BaseTests[PluginT], Generic[PluginT], metaclass=ABCMeta):
         return plugin
 
 
-class PluginIntegrationTests(BaseIntegrationTests[PluginT], Generic[PluginT], metaclass=ABCMeta):
+class PluginIntegrationTests[PluginT](BaseIntegrationTests[PluginT], metaclass=ABCMeta):
     """Integration testing information for basic plugin test classes"""
 
 
-class PluginUnitTests(BaseUnitTests[PluginT], Generic[PluginT], metaclass=ABCMeta):
+class PluginUnitTests[PluginT](BaseUnitTests[PluginT], metaclass=ABCMeta):
     """Unit testing information for basic plugin test classes"""
 
 
-class EnvironmentTests(PluginTests[EnvironmentT], Generic[EnvironmentT], metaclass=ABCMeta):
+class EnvironmentTests[EnvironmentT](PluginTests[EnvironmentT], metaclass=ABCMeta):
     """Shared functionality between the different testing categories"""
+
+    @pytest.fixture(
+        name="environment_type",
+        scope="session",
+        params=environment_variants,
+    )
+    def fixture_environment_type(self, request: pytest.FixtureRequest) -> type[Environment]:
+        """Fixture defining all testable variations mock Environment
+
+        Args:
+            request: Parameterization list
+
+        Returns:
+            Variation of a Environment
+        """
+        environment_type = cast(type[Environment], request.param)
+
+        return environment_type
